018fdd7eca97082c00cdc851aedddcd041a9fab0
jh8oh <ohjiho.ca@gmail.com>
2021-10-15 23:48:41 -0400

    Home HTML

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,33 @@
 <template>
-  <div class="home"></div>
+  <div id="home">
+    <div id="background-image" />
+    <div id="greeting">
+      <span>Hi, I'm</span>
+      <span id="name">Ji Ho Oh</span>
+      <span>Software Developer, Programmer, Designer</span>
+      <br /><br />
+      <Button
+        :text="'Get to know me'"
+        :buttonClass="'secondary'"
+        @clicked="goToAbout()"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { Vue } from "vue-class-component";
+import { Options, Vue } from "vue-class-component";
+import router from "@/router";
+import Button from "@/components/Button.vue";
 
-export default class Home extends Vue {}
+@Options({
+  components: {
+    Button,
+  },
+})
+export default class Home extends Vue {
+  private goToAbout() {
+    router.push("/about");
+  }
+}
 </script>
