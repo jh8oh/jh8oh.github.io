f47200f83f1258f638e2dbe84f5cd8948bbcac01
jh8oh <ohjiho.ca@gmail.com>
2021-10-15 20:17:01 -0400

    Added in home navigation

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,9 +1,31 @@
 <template>
-  <header></header>
+  <header>
+    <nav>
+      <router-link to="/">
+        <img
+          @mouseover="logoHovered = true"
+          @mouseleave="logoHovered = false"
+          :src="logo"
+          alt="Logo"
+        />
+      </router-link>
+    </nav>
+  </header>
 </template>
 
 <script lang="ts">
-import { Vue } from "vue-class-component";
+import { Options, Vue } from "vue-class-component";
+import logoWhite from "@/assets/ico/header/logo-white.png";
+import logoAccent from "@/assets/ico/header/logo-accent.png";
 
-export default class Header extends Vue {}
+@Options({
+  computed: {
+    logo() {
+      return this.logoHovered ? logoAccent : logoWhite;
+    },
+  },
+})
+export default class Header extends Vue {
+  private logoHovered = false;
+}
 </script>
