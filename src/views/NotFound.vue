1da66474d2237346c0b6758f1d8a457940958855
jh8oh <ohjiho.ca@gmail.com>
2021-10-19 19:42:59 -0400

    Added in 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,9 +1,25 @@
 <template>
-  <div></div>
+  <div id="not-found" class="page">
+    <div id="not-found-content">
+      <span class="display">404</span>
+      <br /><br />
+      <Button :text="'Back to home'" :buttonClass="'secondary no-border'" @clicked="goToHome()" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { Vue } from "vue-class-component";
+import { Options, Vue } from "vue-class-component";
+import Button from "@/components/Button.vue";
 
-export default class NotFound extends Vue {}
+@Options({
+  components: {
+    Button,
+  },
+})
+export default class NotFound extends Vue {
+  private goToHome() {
+    this.$router.push("/");
+  }
+}
 </script>
