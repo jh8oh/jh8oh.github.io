eed99778be99eaad78472c79a6110f134e24621f
jh8oh <ohjiho.ca@gmail.com>
2021-10-20 01:11:44 -0400

    Added html meta changes

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "Ji Ho Oh";
+      return args;
+    });
+
     config.module
       .rule("pdf")
       .test(/\.(pdf)(\?.*)?$/)
