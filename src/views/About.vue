d9fe1713ff3b2803b86ca2447745786a5a32c913
jh8oh <ohjiho.ca@gmail.com>
2021-10-20 01:37:33 -0400

    Added about intro

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="page">
+  <div id="about" class="page">
+    <div id="about-content" class="page-content">
+      <h1>About Me</h1>
+      <section id="intro">
+        <img src="../assets/img/about/profile-pic.jpg" alt="Profile Picture" />
+        <p>
+          Hello! I'm Ji Ho Oh (오지호) but I also go by Jay. I'm a 3rd-year undergrad at the
+          University of Waterloo.<br /><br />
+          I like creating user-facing software mostly because I want the things I make to look
+          <span><em>a e s t h e t i c</em></span
+          >. This has led to me building websites, Android applications, and games. At the moment, I
+          am focusing on learning new libraries and frameworks over specializing in one specific
+          aspect. However, I am also looking for a field that might capture my interest and allow me
+          to dive deeper.<br /><br />Aside from coding, my hobbies include playing video games,
+          reading manga, and world-building.
+        </p>
+      </section>
+    </div>
     <Footer />
   </div>
 </template>
