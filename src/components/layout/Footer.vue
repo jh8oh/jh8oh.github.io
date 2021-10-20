52bd9b66880305d14f766bedec087ec89ebfa281
jh8oh <ohjiho.ca@gmail.com>
2021-10-19 23:45:32 -0400

    Created Footer

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="body2">© 2020 Ji Ho Oh</footer>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class Footer extends Vue {}
+</script>
