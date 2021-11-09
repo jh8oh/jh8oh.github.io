25f962d1f8d5b0c20ccb61b70d9fb7cb93d8b2f4
jh8oh <ohjiho.ca@gmail.com>
2021-11-09 16:38:20 -0500

    Created dexgraph project

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -2,9 +2,15 @@
   <div id="portfolio" class="page">
     <div id="portfolio-content" class="page-content">
       <h1>Portfolio</h1>
+      <section id="featured">
+        <ul>
+          <li v-for="project in featuredProjects" :key="project.title"></li>
+        </ul>
+      </section>
       <section id="archive">
+        <h2>Archive</h2>
         <ul id="archive-projects">
-          <li v-for="project in archivedProjects" :key="project.id">
+          <li v-for="project in archivedProjects" :key="project.title">
             <ArchivedProjectCard :project="project" />
           </li>
         </ul>
@@ -35,6 +41,7 @@ import {
   },
 })
 export default class Portfolio extends Vue {
+  private featuredProjects = [];
   private archivedProjects = [
     oneStoryADay,
     weekPlanner,
